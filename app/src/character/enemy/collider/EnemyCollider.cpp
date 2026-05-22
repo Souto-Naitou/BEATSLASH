0754c8db4f329abd690b44672712b567ea32f61e
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2026-05-22 14:09:40 +0900

    敵の衝突判定の追加

---
+++ app/src/character/enemy/collider/EnemyCollider.cpp
@@ -0,0 +1,38 @@
+#include "EnemyCollider.h"
+#include <utility/CollisionUtility.h>
+#include <type/ColliderTypeID.h>
+
+void EnemyCollider::OnCollisionEnter(Collider* other)
+{
+	ColliderTypeID otherID = static_cast<ColliderTypeID>(other->GetTypeID());
+	if (otherID == ColliderTypeID::Player)
+	{
+		// プレイヤーに当たったら一旦押し戻しを行う
+		Tako::Vector3 pushback = CollisionUtility::CalcPushback(this, other);
+		// エネミーに押し戻しのコールバックが設定されていれば呼び出す
+		if (pushBackCallback_)
+		{
+			pushBackCallback_(pushback);
+		}
+	}
+}
+
+void EnemyCollider::OnCollisionStay(Collider* other)
+{
+	ColliderTypeID otherID = static_cast<ColliderTypeID>(other->GetTypeID());
+	if (otherID == ColliderTypeID::Player)
+	{
+		// プレイヤーに当たったら一旦押し戻しを行う
+		Tako::Vector3 pushback = CollisionUtility::CalcPushback(this, other);
+		// エネミーに押し戻しのコールバックが設定されていれば呼び出す
+		if (pushBackCallback_)
+		{
+			pushBackCallback_(pushback);
+		}
+	}
+}
+
+void EnemyCollider::OnCollisionExit(Collider* other)
+{
+
+}
